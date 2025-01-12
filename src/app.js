1bcd06d7ebb8915a71bb7c63d6f9f77d603d221f
prathameysh <prathameshgavatre@gmail.com>
2025-01-12 14:34:09 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express= require('express');
-
 const app= express();
 
-app.use("/",(req, res)=>{
-    res.send("Hello World from Server from home page");
+//only handle GET call to /user
+app.get("/user",(req,res) => {
+    res.send({firstName:"Prat", lastName:"Gavatre"});
 });
 
-app.use("/test",(req, res)=>{
-    res.send("Hello World from Server from test page");
+app.post("/user",async(req,res) => {
+    console.log(req.body);
+    //saving data to DB
+    res.send("data sent successfully to database");
 });
 
+app.delete("/user",(req,res) => {
+    res.send("data deleted successfully from database");
+});
+
+//all the HTTP method API calls to /hello
 app.use("/hello",(req, res)=>{
-    res.send("Hello World from Server from hello");
+    res.send("Hello World from Server");
 });
 
 app.listen(3000,() =>{
