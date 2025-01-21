4b533171b2555398b6ade954001340ca6e1176dc
prathameysh <prathameshgavatre@gmail.com>
2025-01-21 17:43:01 +0530

    API connection request accept/reject

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,7 +8,7 @@ const userAuth = async (req, res, next) => {
       throw new Error("Token is not valid!!!!!!!!!");
     }
 
-    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+    const decodedObj = await jwt.verify(token, "Prat@2004");
 
     const { _id } = decodedObj;
 
