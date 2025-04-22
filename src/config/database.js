ea5704a9cc6a78f0526626dde8bbf2863577447b
prathameysh <prathameshgavatre@gmail.com>
2025-04-22 18:16:32 +0530

    mongo string updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
-const mongoose =require ("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB= async ()=>{
-    await mongoose.connect("mongodb+srv://prathameshgavatre:ishwari.2009@namastenode.5rc03jd.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode");
-    console.log('MongoDB Connected');
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://prathameshgavatre:abc123def@namastenode.5rc03jd.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode"
+  );
+  console.log("MongoDB Connected");
 };
 
-module.exports= connectDB;
+module.exports = connectDB;
